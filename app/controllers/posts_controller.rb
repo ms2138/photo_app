396415b6c61b941583aff7cd5b579d68bc89dd39
ms2138 <m38371@gmail.com>
2022-02-12 23:28:09 -0800

    Add functionality to allow the creation of new posts and uploading of images using dropzone

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: [:index, :show, :new, :create, :destroy]
+
   def index
   end
 
@@ -6,5 +8,28 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = @user.posts.new
   end
+
+  def create
+    @post = @user.posts.build(post_params)
+    respond_to do |format| 
+      if @post.save
+        format.html { redirect_to user_posts_path(@user), notice: "Post was successfully created." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, images: [])
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :posts, only: [:show, :index, :new, :create]
+  resources :users, only: [:show, :index, :destroy] do
+    resources :posts, only: [:show, :index, :new, :create]
+  end
 
   root "posts#index"
 end
