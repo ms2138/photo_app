0a0b9acc6fc610626e4d86ac963c1138c6fed628
ms2138 <m38371@gmail.com>
2022-02-15 21:58:00 -0800

    Initialize Pundit in ApplicationController.  Add a redirect if user is not authorized to access page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!    
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    include Pundit
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name, :website, :bio])
+    end
+
+    private
+
+    def user_not_authorized
+        redirect_to root_path, alert: 'You are not authorized to perform this action.'
     end
 end
