e26b5570a17eb089bd6d09807cbc08c144f718b8
ms2138 <m38371@gmail.com>
2022-02-20 21:11:15 -0800

    Add RegistrationsController.  Add after_update_path_for method to redirect after editing user profile information.  Update devise user routes to point to RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'signin', sign_out: 'signout', sign_up: 'signup'}
+  devise_for :users, path: '', path_names: { sign_in: 'signin', sign_out: 'signout', sign_up: 'signup'},
+             :controllers => { :registrations => :registrations }
 
   resources :users, only: [:show, :index, :destroy] do
     resources :posts, only: [:show, :index, :new, :create]
