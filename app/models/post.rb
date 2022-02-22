1ebf1f6108484754acd048712e2cb4b2ef84ab39
ms2138 <m38371@gmail.com>
2022-02-21 20:57:53 -0800

    Add comments has_many relation to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-
   has_many_attached :images
+  has_many :comments, -> { order('created_at desc') }, dependent: :destroy
 
   validates :content, presence: true
 end
