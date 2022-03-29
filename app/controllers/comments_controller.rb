83b7405f8cb64a46b9cf0627782ee0602d1b169c
ms2138 <m38371@gmail.com>
2022-03-28 21:27:00 -0700

    Update comment create/destroy to use turbo streams

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,22 @@
 class CommentsController < ApplicationController
+    before_action :set_user, only: [:new, :create]
     after_action :verify_authorized
 
-    def index
-        @comments = authorize @post.comments.includes(:user)
-    end
-
     def new
-        @comment = authorize current_user.comments.new
+        @comment = authorize @user.comments.new
     end
     
     def create
-        @comment = authorize current_user.comments.build(comment_params)
+        @comment = authorize @user.comments.build(comment_params)
         respond_to do |format| 
             if @comment.save
                 @post = @comment.post
-                format.js
+                format.turbo_stream do
+                   render turbo_stream:  [
+                    turbo_stream.update('new_comment', partial: 'comments/form', locals: { user: @user, post: @post, comment: Comment.new } ),
+                    turbo_stream.prepend('comments', partial: 'comments/comment', locals: { user: @user, post: @post, comment: @comment } )
+                ]
+                end
             else
                 format.html { redirect_to user_post_path(current_user, @post), alert: "Failed to create comment." }
             end
@@ -24,10 +26,12 @@ class CommentsController < ApplicationController
     def destroy
         @comment = authorize Comment.find(params[:id])
         @post = @comment.post
-        if @comment.destroy
-            respond_to :js
-        else
-            format.html { redirect_to user_post_path(current_user, @post), alert: "Failed to create comment." }
+        respond_to do |format| 
+            if @comment.destroy
+                format.turbo_stream { render turbo_stream: turbo_stream.remove(@comment) }
+            else
+                format.html { redirect_to user_post_path(current_user, @post), alert: "Failed to create comment." }
+            end
         end
     end
 
@@ -35,5 +39,10 @@ class CommentsController < ApplicationController
 
     def comment_params
         params.require(:comment).permit(:user_id, :post_id, :content)
+    end
+    
+    def set_user
+        @user =  User.find_by(id: params[:user_id])
+        redirect_to root_url if @user.nil?
     end
 end
