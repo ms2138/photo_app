a9b3ac9860762bdb918d22c34c97f19491d6a753
ms2138 <m38371@gmail.com>
2022-04-02 21:35:36 -0700

    Add has_many relationships for following and followers in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   acts_as_voter
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,4 +16,15 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 40 }
   validates :name, presence: true, length: { maximum: 100 }
   
+  def follow(user)
+    following << user
+  end
+
+  def unfollow(user)
+    following.delete(user)
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
 end
