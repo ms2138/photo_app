bfa1352cab7a99c5c4e866009c24348b1a6be0ec
ms2138 <m38371@gmail.com>
2022-04-02 23:07:29 -0700

    Add edit, following and followers methods to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
+  before_action :set_user, only: [:show, :edit, :destroy]
   after_action :verify_authorized
 
   def index
@@ -8,13 +8,26 @@ class UsersController < ApplicationController
 
   def show
       @posts = Post.includes(:user).where(user_id: @user.id).limit(5).order("created_at").page(params[:page]).per(3)
-    end
+  end
+
+  def edit
+  end
 
   def destroy
       @user.destroy
       respond_to do |format|
         format.html { redirect_to users_path, flash: { success: "User successfully deleted." } }
       end
+  end
+
+  def following
+    @user = authorize User.find_by(id: params[:id])
+    @following = @user.following.order("created_at").page(params[:page]).per(3)
+  end
+
+  def followers
+    @user = authorize User.find_by(id: params[:id])
+    @followers = @user.followers.order("created_at").page(params[:page]).per(3)
   end
 
   private
