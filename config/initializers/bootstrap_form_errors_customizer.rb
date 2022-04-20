287726f206d79d6fa27ffa9436b856eff839048e
ms2138 <m38371@gmail.com>
2022-04-19 22:43:17 -0700

    Update bootstrap form errors to hightlight fields where validations failed

--- config/initializers/bootstrap_form_errors_customizer.rb
+++ config/initializers/bootstrap_form_errors_customizer.rb
@@ -1,10 +1,26 @@
 ActionView::Base.field_error_proc = proc do |html_tag, instance|
-    class_attr_index = html_tag.index 'class="'
-    class_attr_length = 'class="'.length
-  
-    if html_tag =~ /^<label/
-      html_tag.html_safe
+  is_label_tag = html_tag =~ /^<label/
+  class_attr_index = html_tag.index 'class="'
+
+  def format_error_message_to_html_list(error_msg)
+    html_list_errors = '<ul></ul>'
+    if error_msg.is_a?(Array) || error_msg.is_a?(ActiveModel::DeprecationHandlingMessageArray)
+      error_msg.each do |msg|
+        html_list_errors.insert(-6, "<li>#{msg}</li>")
+      end
     else
-      html_tag.insert(class_attr_index + class_attr_length, 'is-invalid ')
+      html_list_errors.insert(-6, "<li>#{msg}</li>")
     end
+    html_list_errors
+  end
+
+  if class_attr_index && !is_label_tag
+    html_tag.insert(class_attr_index + 7, 'is-invalid ')
+    html_tag
+  elsif !class_attr_index && !is_label_tag
+    html_tag.insert(html_tag.index('>'), ' class="is-invalid"')
+    html_tag
+  else
+    html_tag.html_safe
+  end
 end
